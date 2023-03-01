8f987aed23d1bab82383d61dd8a6ee0806de592f
João Moura <79586002+JohnnyMinho@users.noreply.github.com>
2023-03-01 20:12:57 +0000

    Add files via upload
    Emissor atualizado 1/3

--- Emissor/Emissor.ino
+++ Emissor/Emissor.ino
@@ -1,14 +1,17 @@
 #include <printf.h>
 #include <stdio.h>
 #include "Tipos.h"
-
+#include "CRCx.h";
 
 #define SendingPin 32//Define o Pino pelo qual irá ser usado como pino de envio de dados
 //Um pino que não têm nenhum informação importante para qualquer protocolo é o pino 32, um que apenas têm como funções extra ser touch sensitive
 
-int bloco_sync[20] = {1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0}; //bloco em formato numérico visto que este é transformado em bytes através de shifting
-byte cabecalho[33];
-byte cauda[18];
+int bloco_sync[10] = {1, 1, 0, 1, 1, 1, 1, 0, 1, 1}; //bloco em formato numérico visto que este é transformado em bytes através de shifting
+byte cabecalho[6];
+byte tag_fluxo[1]; //Identificação do grupo de fluxo a que uma trama pertence, ou seja, como a trama da parte de uma mensagem é retransmitida 3 vezes, é identificado o grupo de tramas a qual esta pertençe
+byte tag_msg[1]; //Identificação da mensagem quanto à sua posição no seu grupo de fluxo
+byte cauda[17];
+byte tamanho_msg[1];
 byte* dados; //até 255 bytes
 //Variável cabeçalho -
 //Variável cauda
@@ -16,14 +19,14 @@ byte* dados; //até 255 bytes
 
 //Definimos o modo em que o pino escolhido como emissor vai trabalhar
 
-byte* formular_cabecalho() {
-  byte sync_block[3]; //bloco de sincronização no qual 20 bits vão servir para sincronizar ambos os elementos, ou seja, ao receber uma mensagem o receptor vai começar por igualar todos os valores através de um ciclo.
+byte* formular_cabecalho(int grupo_fluxo_envio2,byte tmnh_msg,byte n_envio) {
+  byte sync_block[2]; //bloco de sincronização no qual 20 bits vão servir para sincronizar ambos os elementos, ou seja, ao receber uma mensagem o receptor vai começar por igualar todos os valores através de um ciclo.
   byte tipo_trama;
   sync_block[3] = '\0';
   int i = 0; //varíavel temp para rodar o bloco de sync
   int x = 0; //varíavel para fazer shifting dos bits do bloco de sync
-  for (i = 0; i < 3; i++){
-    for (x = 0 + x * i; x < 8 * i; x++){
+  for (i = 0; i < 3; i++) {
+    for (x = 0 + x * i; x < 8 * i; x++) {
       sync_block[i] |= ~(bloco_sync[x] << x);
     }
   }
@@ -33,40 +36,46 @@ byte* formular_cabecalho() {
       tipo_trama = HELLO;
       break;
     case 1:
-      tipo_trama = Sync;
-      break;
-    case 2:
       tipo_trama = Dados0;
       break;
-    case 3:
+    case 2:
       tipo_trama = Dados1;
       break;
-    case 4:
+    case 3:
       tipo_trama = Dados2;
       break;
   }
+  byte grupo_fluxo_byte = byte(grupo_fluxo_envio2);
+  memcpy(cabecalho,sync_block,2*sizeof(byte));
+  memcpy(cabecalho,tipo_trama,sizeof(sync_bloc)+sizeof(cabecalho));
+  memcpy(cabecalho,tmnh_msg,sizeof(tmnh_msg)+sizeof(cabecalho));
+  memcpy(cabecalho,grupo_fluxo_byte,sizeof(grupo_fluxo_byte)+sizeof(cabecalho));
+  memcpy(cabecalho,grupo_msg_byte,sizeof(grupo_msg_byte)+sizeof(cabecalho));
+
+  return cabec;
+}
+
+boolean formar_enviar_pacote(byte* cabec, byte* CRC_part, byte* mensagem,int grupo_fluxo_envio){
+  int n_reenvio = 0;
   int tamanho_msg = 0;
+  
   dados = (byte*) malloc(tamanho_msg * sizeof(byte));
-  for(int o=0;o<tamanho_msg; o++){
-    
+  for (int o = 0; o < tamanho_msg; o++) {
+    dados[o] = mensagem[o];
   }
-  byte start_bit = 0b00000001;
+  uint16_t result16 = crcx::crc16(dados, tamanho_msg); //Formulação do CRC ou seja da cauda da trama
+  byte tamanho_msg_byte = byte(tamanho_msg);
+  byte n_reenvio_byte = byte(n_reenvio);
+  byte* pacote = (byte*) malloc((4 + tamanho_msg + sizeof(CRC_part)) * sizeof(byte));
+  byte* cabec_pacote = formular_cabecalho(grupo_fluxo_envio,tamanho_msg_byte, n_reenvio_byte);
+  memcpy(pacote, cabec_pacote,sizeof(cabec_pacote));
+  memcpy(pacote,dados,sizeof(dados)+sizeof(dados));
+  memcpy(pacote,CRC_part,sizeof())
+  free(pacote);
   free(dados); //TÊMOS DE LIBERTAR A MEMÓRIA EM C , NÃO SE ESQUEÇAM
 }
 
-byte* formular_cauda() {
-
-}
-
-byte* formar_pacote() {
-
-}
-
 byte* formar_CRC() {
-
-}
-
-void enviar(byte* mensagem) {
 
 }
 
@@ -76,5 +85,6 @@ void setup() {
 }
 
 void loop() {
+  int grupo_fluxo = 0;
 
 }
--- Emissor/Tipos.h
+++ Emissor/Tipos.h
@@ -1,10 +1,10 @@
 #ifndef INTINFO_H_
 #define INTINFO_H_
 
-byte HELLO = 0b00000000;
-byte Sync = 0b00000001;
-byte Dados0 = 0b00000010; //Texto
-byte Dados1 = 0b00000011; //Ficheiro
-byte Dados2 = 0b00000100; //Imagem
+int* HELLO = 0b00000000;
+int* Sync = 0b00000001;
+int* Dados0 = 0b00000010;
+int* Dados1 = 0b00000011;
+int* Dados2 = 0b00000100;
 
 #endif
